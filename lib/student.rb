ad684ad2a0fa4dac0c990a49ef07751d9413d519
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-15 22:25:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,7 +83,7 @@ class Student
       FROM students
       WHERE grade = ?
     SQL
-    DB[:conn].execute(sql, arg).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end
